665484a5917d24929caec8878e3f94e5b6776e2c
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-07-06 23:57:07 +0900

    add string example

---
+++ app/src/c2/quote-str.py
@@ -0,0 +1,11 @@
+# シングルクォートを文字列で使いたい場合
+print("I can't speak English.")
+
+# ダブルクォートを文字列で使いたい場合
+print('He said, "I play the piano.".')
+
+# シングルクォートの中でシングルクォートを表現
+print('I can\'t play the guitar.')
+
+# ダブルクォートの中でダブルクォートを表現
+print("I like \"Orange\".")
