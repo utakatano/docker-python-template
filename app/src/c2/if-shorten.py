88cd1e05cc07957fc6e81f88200a11e8557b17a9
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-07-13 23:56:57 +0900

    add if-statement examples

---
+++ app/src/c2/if-shorten.py
@@ -0,0 +1,8 @@
+v = 30
+
+# (1) normal if-statement
+if v % 2 == 0:
+    print("even")
+
+# (2) shorten if-statement
+if v % 2 == 0: print("even")
--- app/src/c2/leapyear.py
+++ app/src/c2/leapyear.py
@@ -4,18 +4,20 @@ year = int(input("what year? "))
 is_leap = False
 
 # (1) leap year if it is divided by 4
-if year % 4 == 0:
-    # (2) not leap year if it is divided by 100
-    if year % 100 == 0:
-        # (3) leap year if it is divided by 400
-        if year % 400 == 0:
-            is_leap = True
-        else:
-            is_leap = False
-    else:
-        is_leap = True
-else:
-    is_leap = False
+# if year % 4 == 0:
+#     # (2) not leap year if it is divided by 100
+#     if year % 100 == 0:
+#         # (3) leap year if it is divided by 400
+#         if year % 400 == 0:
+#             is_leap = True
+#         else:
+#             is_leap = False
+#     else:
+#         is_leap = True
+# else:
+#     is_leap = False
+
+is_leap = (year % 400 == 0) or ((year % 100 != 0) and (year % 4 == 0))
 
 # display the result
 if is_leap:
