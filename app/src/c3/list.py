fdefaede7adaaae1ec04eba45929247c0e7c25ca
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-08-02 23:57:11 +0900

    create list examples

---
+++ app/src/c3/list.py
@@ -0,0 +1,12 @@
+a = [10, 22, 30, 45]
+print(a)
+
+print(a[0])
+
+print(a[3])
+
+a[1] = 555
+
+print(a)
+
+print(len(a))
---
+++ app/src/c3/test-sum.py
@@ -0,0 +1,10 @@
+points = [88, 76, 67, 43, 79, 80, 91]
+
+sum_v = 0
+
+for i in points:
+    sum_v += i
+    print("Add ", i, " and total is ", sum_v)
+
+ave_v = sum_v / len(points)
+print("Average is ", ave_v, " points")
