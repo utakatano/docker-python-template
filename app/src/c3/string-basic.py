363dd906be245a9891e6defba799209947a49513
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-08-10 23:56:54 +0900

    add string slice and split examples

--- app/src/c3/string-basic.py
+++ app/src/c3/string-basic.py
@@ -28,3 +28,35 @@ t = "abcd"
 print(t[0])
 
 print(t[2])
+
+st = "abcdefg"
+
+print(st[1:4])
+
+print(st[2:3])
+
+n = '0123456789'
+
+print(n[2:5])
+
+print(n[4:9])
+
+print(n[5:8])
+
+print(n[3:3])
+
+print(n[:3])
+
+print(n[7:])
+
+print(n[-1])
+
+print(n[-3:])
+
+print(n[0:9:2])
+
+print(n[::3])
+
+text = "This is a pen."
+
+print(text.split())
