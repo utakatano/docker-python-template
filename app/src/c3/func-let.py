bee26e191fe7398408329e9593f44a0c4f002a08
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-08-24 23:33:42 +0900

    substitute-for-function

---
+++ app/src/c3/func-let.py
@@ -0,0 +1,15 @@
+def mul_func(a, b):
+    return a * b
+
+def div_func(a, b):
+    return a / b
+
+func = mul_func
+result = func(2, 3)
+
+print(result)
+
+func2 = div_func
+result = func2(10, 5)
+
+print(result)
