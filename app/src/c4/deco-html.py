5883e19c265dffcdea906d19164bf5b9fb18f838
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-09-22 23:35:44 +0900

    add decorator examples

---
+++ app/src/c4/deco-html.py
@@ -0,0 +1,22 @@
+def wrap_html(func):
+    def wrapper(*args, **kwargs):
+        s = "<html>"
+        s += func(*args, **kwargs)
+        s += "</html>"
+        return s
+    return wrapper
+
+def wrap_body(func):
+    def wrapper(*args, **kwargs):
+        s = "<body>"
+        s += func(*args, **kwargs)
+        s += "</body>"
+        return s
+    return wrapper
+
+@wrap_html
+@wrap_body
+def show_html(text):
+    return text
+
+print(show_html("デコレータのテスト"))
